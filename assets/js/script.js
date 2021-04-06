3425a13084dcffd7f7a157f5f3ee4af63a5da1c4
Daniel <dkbaffour777@gmail.com>
2021-04-05 23:00:33 -0700

    mark scores, update/delete high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ barEl.innerHTML =  `
         View high scores
     </div>
     <div>
-        Time: <span id="time">0</span>
+        Time: <span id="time">0</span>s left
     </div>
 `
 
@@ -24,39 +24,28 @@ welcomeEl.innerHTML =  `
 
 var highScoreBoardEl = document.createElement("section")
 highScoreBoardEl.setAttribute("id", "high-score-board")
-highScoreBoardEl.innerHTML =  `
-    <h1>High Scores</h1>
-    <ul>
-        <li>1. AB-33</li>
-        <li>2. AY-73</li>
-        <li>3. HF-33</li>
-    </ul>
-    <div class="high-score-btns">
-        <button id="go-back-btn" class="btn" type="submit">Go back</button>
-        <button id="clear-high-scores" class="btn">Clear high scores</button>
-    </div>
-`
+highScoreBoardEl.innerHTML = `
+        <h1>High Scores</h1>
+        <ul id="high-score-cont">
+        </ul>
+        <div class="high-score-btns">
+            <button id="go-back-btn" class="btn" type="submit">Go back</button>
+            <button id="clear-high-scores" class="btn">Clear high scores</button>
+        </div>
+    `
 
 var questionsWrapperEl = document.createElement("section")
 questionsWrapperEl.setAttribute("id", "questions-wrapper")
 
 var questionEl = document.createElement("div")
 questionEl.setAttribute("id", "question")
-questionEl.innerHTML = `
-    <h2>Commonly used data types Do Not include?</h2>
-    <ul>
-        <li>1. beans</li>
-        <li>2. rice</li>
-        <li>3. bread</li>
-    </ul>
-`
 
 var allDoneEl = document.createElement("section")
 allDoneEl.setAttribute("id", "all-done")
 allDoneEl.innerHTML = `
-    <h1>All done!</h1>
-    <p>Your final score is <span id="final-score">23</span></p>
-    <form>
+    <h1 id="all-done-msg">All done!</h1>
+    <p>Your final score is <span id="final-score"></span> /100</p>
+    <form id="all-done-form">
         <div id="input-wrapper">
             <label>Enter Initials: </label>
             <input type="text" name="initials">
@@ -69,20 +58,135 @@ var questionMsgWrapperEl = document.createElement("div")
 questionMsgWrapperEl.setAttribute("id", "question-msg-wrapper")
 questionMsgWrapperEl.innerHTML = `
     <hr>
-    <div id="question-msg">Correct</div>
+    <div id="question-msg"></div>
 `
+
+var appNavigationHistory = []
+var functionCall = {handleStartBtn: false, handleGoBackBtn: false, handleViewHighScore: false, handleAllDoneSubmit: false}
+var timerInterval, timer = 0
+
+var javascriptQuestionsID = 0, score = 0
+var javascriptQuestions = [
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        options: [
+            {
+                option: "numbers and strings",
+                answer: false
+            },
+            {
+                option: "other arrays",
+                answer: false
+            },
+            {
+                option: "booleans",
+                answer: false
+            },
+            {
+                option: "all of the above",
+                answer: true
+            }
+        ]
+    },
+    {
+        question: "Commonly used data types DO Not include:",
+        options: [
+            {
+                option: "strings",
+                answer: false
+            },
+            {
+                option: "alerts",
+                answer: true
+            },
+            {
+                option: "booleans",
+                answer: false
+            },
+            {
+                option: "numbers",
+                answer: false
+            }
+        ]
+    },
+    {
+        question: "The condition in an if / else statment is enclosed with _______.",
+        options: [
+            {
+                option: "square brackets",
+                answer: false
+            },
+            {
+                option: "curly brackets",
+                answer: false
+            },
+            {
+                option: "quotes",
+                answer: false
+            },
+            {
+                option: "parenthesis",
+                answer: true
+            }
+        ]  
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            {
+                option: "console.log",
+                answer: true
+            },
+            {
+                option: "for loops",
+                answer: false
+            },
+            {
+                option: "terminal/bash",
+                answer: false
+            },
+            {
+                option: "Javascript",
+                answer: false
+            }
+        ]
+    },
+    {
+        question: "Strings must be enclosed within _______ when being assigned to variables.",
+        options: [
+            {
+                option: "commas",
+                answer: false
+            },
+            {
+                option: "curly brackets",
+                answer: false
+            },
+            {
+                option: "quotes",
+                answer: true
+            },
+            {
+                option: "parenthesis",
+                answer: false
+            }
+        ]  
+    }
+]
+
+
 var questionAllDonePage = function(element) {
     mainEl.innerHTML = ""
     mainEl.appendChild(barEl)
     mainEl.appendChild(questionsWrapperEl)
+    questionsWrapperEl.innerHTML = ""
     questionsWrapperEl.appendChild(element)
     questionsWrapperEl.appendChild(questionMsgWrapperEl)
-    handleViewHighScore()
+    if(!functionCall.handleViewHighScore) {
+        handleViewHighScore()
+        functionCall.handleViewHighScore = true
+    }
 }
-
-var appNavigationHistory = []
-var functionCall = {handleStartBtn: false, handleGoBackBtn: false, handleViewHighScore: false}
-var timerInterval, timer = 5
 
 var appNavigation = {
     welcomePage: function() {
@@ -106,6 +210,7 @@ var appNavigation = {
         }
     },
     questionsPage: function() {
+        questionEl.innerHTML = renderQuestion(javascriptQuestionsID)
         questionAllDonePage(questionEl)
         appNavigationHistory.push("questionsPage")
         localStorage.setItem("appNavigationHistory", JSON.stringify(appNavigationHistory))
@@ -114,13 +219,19 @@ var appNavigation = {
         questionAllDonePage(allDoneEl)
         appNavigationHistory.push("allDonePage")
         localStorage.setItem("appNavigationHistory", JSON.stringify(appNavigationHistory))
+        if(!functionCall.handleAllDoneSubmit) {
+            handleAllDoneSubmit()
+            functionCall.handleAllDoneSubmit = true
+        }
     },
     highScorePage: function() {
         mainEl.innerHTML = ""
         mainEl.appendChild(highScoreBoardEl)
+        document.querySelector("#high-score-cont").innerHTML = renderHighScore()
 
         if(!functionCall.handleGoBackBtn) {
             handleGoBackBtn()
+            handleClearHighScores()
             // change the value of handleGoBackBtn to true when it's corresponding function is called
             functionCall.handleGoBackBtn = true
         }
@@ -131,6 +242,59 @@ var appNavigation = {
 
 // Render the welcome page
 appNavigation.welcomePage()
+
+// Main elements events
+mainEl.addEventListener("click", dispatchEventMainEl)
+
+function dispatchEventMainEl(event){
+    var targetEl = event.target
+    if(targetEl.matches(".option")) {
+        var questionID = targetEl.getAttribute("data-question-id")
+        var optionID = targetEl.getAttribute("data-option-id")
+        var answer = javascriptQuestions[questionID].options[optionID].answer
+        console.log(javascriptQuestions[questionID].options[optionID].answer)
+        
+        if(javascriptQuestionsID < javascriptQuestions.length) {
+            // Update the next question
+            if(answer) {
+                score += 20
+                console.log("js id: ", javascriptQuestionsID)
+                console.log("Your score is now: ", score)
+                document.querySelector("#question-msg").textContent = "Correct!"
+            } else {
+                timer -= 10
+                document.querySelector("#question-msg").textContent = "Wrong!"
+            }
+            
+            // Go to the next javascript question index
+            javascriptQuestionsID++
+
+            if(javascriptQuestionsID <= javascriptQuestions.length - 1) {
+                questionsWrapperEl.innerHTML = renderQuestion(javascriptQuestionsID)
+                questionsWrapperEl.appendChild(questionMsgWrapperEl)
+            } else {
+                appNavigation.allDonePage()
+                document.querySelector("#final-score").textContent = score
+                clearInterval(timerInterval)
+            }
+        }
+    }
+}
+
+// Render the questions
+function renderQuestion(questionID) {
+    var list = ""
+    for (let optionID = 0; optionID < javascriptQuestions[questionID].options.length; optionID++) {
+        list += "<li class=option data-option-id=" + optionID + " data-question-id=" + questionID + ">" 
+            + javascriptQuestions[questionID].options[optionID].option + "</li>"
+    }
+    return `
+        <h2>` + javascriptQuestions[questionID].question + `</h2>
+        <ul>
+            ` + list + `
+        </ul>
+    `
+}
 
 // Render the high scores page
 function handleViewHighScore() {
@@ -147,15 +311,19 @@ function handleStartBtn() {
 
 // Handle timer
 function handleTimer() {
+    timer = 75
+    if(mainEl.contains(barEl)) document.querySelector("#time").textContent = timer
     timerInterval = setInterval(function(){
-        console.log(timer)
         // set the time element to timer only when it exists 
-        if(mainEl.contains(barEl)) document.querySelector("#time").textContent = timer
-        
         timer--
+        if(mainEl.contains(barEl)) document.querySelector("#time").textContent = (timer < 0) ? 0 : timer
+        if(timer <= 0) {
+            appNavigation.allDonePage()
+            document.querySelector("#all-done-msg").textContent = "Sorry, time up!"
+            document.querySelector("#final-score").textContent = score
+        }
         if(timer < 0) {
             clearInterval(timerInterval)
-            timer = (timer < 0) ? 0 : timer
         }
      }, 1000)
 }
@@ -183,5 +351,43 @@ function  handleGoBackBtn() {
             appNavigationHistory.pop()
             localStorage.setItem("appNavigationHistory", JSON.stringify(appNavigationHistory))
         }
+    })
+}
+
+// Render high score
+function renderHighScore() {
+    var storedHighScores = JSON.parse(localStorage.getItem("highScores")) 
+    var highScoreEl = ""
+    console.log(storedHighScores)
+    if(storedHighScores){
+        for (let highScore = 0; highScore < storedHighScores.length; highScore++) {
+            highScoreEl += "<li>" + storedHighScores[highScore].initials + " - " + storedHighScores[highScore].score + "</li>"
+        }
+    }
+    return highScoreEl
+}
+
+// Handle all done
+function handleAllDoneSubmit() {
+    var highScores = []
+    document.querySelector("#all-done-form").addEventListener("submit", function(event){
+        var initials = document.querySelector("input[name='initials']").value
+        if(initials.length === 0){
+            alert("Please fill in your initials")
+            event.preventDefault()
+        }else{
+            initials = initials.toUpperCase()
+            highScores.push({initials, score})
+            localStorage.setItem("highScores", JSON.stringify(highScores))
+            alert("Your score was saved")
+        }
+    })
+}
+
+// handle clear high scores
+function handleClearHighScores() {
+    document.querySelector("#clear-high-scores").addEventListener("click", function(){
+        localStorage.setItem("highScores", null)
+        document.querySelector("#high-score-cont").innerHTML = ""
     })
 }
